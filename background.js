36be3a6ed1e2f5b562089483eadc4f1c52d3b444
Ali Sardari <ali-sardari@live.com>
2024-09-24 22:20:38 +0200

    - fix service worker

--- background.js
+++ background.js
@@ -1,17 +1,3 @@
-// Create a MediaQueryList object
-const mediaQueryList = window.matchMedia('(max-width: 600px)');
-
-// Define the callback function
-function handleMediaQueryChange(event) {
-    if (event.matches) {
-        console.log('xxx.The viewport is 600 pixels or less.');
-    } else {
-        console.log('xxx.The viewport is more than 600 pixels.');
-    }
-}
-
-// Add the event listener using addEventListener
-mediaQueryList.addEventListener('change', handleMediaQueryChange);
-
-// Optionally, you can also remove the event listener if needed
-// mediaQueryList.removeEventListener('change', handleMediaQueryChange);
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed');
+});
